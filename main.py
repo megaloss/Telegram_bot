e8ccb350e39e7c2e4ac0094649e09b76eb49a2df
megaloss <megaloss@gmail.com>
2021-10-05 10:24:03 +0200

    added allowed id check

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 import os
 import urllib.request
 import telebot
@@ -5,6 +6,7 @@ from pydub import AudioSegment
 import speech_recognition as sr
 import pytesseract
 from PIL import Image
+ALLOWED_IDS = [1928616895,]
 
 API_KEY = os.environ['TELE_API']
 if not API_KEY:
@@ -22,29 +24,24 @@ def recognize(file):
 
 
 def transcribe(file, chunksize=60000):
-    # 0: load mp3
+    # 0: load
     sound = AudioSegment.from_ogg(file)
-    #sound = AudioSegment.from_mp3(filepath)
 
     # 1: split file into 60s chunks
     def divide_chunks(sound, chunksize):
-        # looping till length l
         for i in range(0, len(sound), chunksize):
             yield sound[i:i + chunksize]
 
     chunks = list(divide_chunks(sound, chunksize))
-    #print(f"{len(chunks)} chunks of {chunksize / 1000}s each")
 
     r = sr.Recognizer()
     # 2: per chunk, save to wav, then read and run through recognize_google()
-    string_index = {}
     string =''
     for index, chunk in enumerate(chunks):
         # TODO io.BytesIO()
         chunk.export('delete_me.wav', format='wav')
         with sr.AudioFile('delete_me.wav') as source:
             audio = r.record(source)
-        # s = r.recognize_google(audio, language="en-US") #, key=API_KEY) --- my key results in broken pipe
         #s = r.recognize_sphinx(audio, language="en-US")
         s = r.recognize_google(audio, language="en-US")
         string += s
@@ -54,16 +51,14 @@ def transcribe(file, chunksize=60000):
 
 @bot.message_handler()#commands=['Greet','greet','hi','Hi','hello','Hello'])
 def greet(message):
-    print (message)
-    print ('message')
-    bot.reply_to(message, f"hi, {message.from_user.first_name} !")
+
+    bot.reply_to(message, f"hi, {message.from_user.first_name} ! Your id is: {message.from_user.id}")
 
 
 @bot.message_handler(content_types=['photo'])
 def photo(message):
-    if message.from_user.id != 1928616895:
+    if message.from_user.id not in ALLOWED_IDS:
         return
-    #print (message)
     bot.reply_to(message, 'Got photo. recognition started...')
     try:
         file_location = message.photo[-1].file_id
@@ -82,7 +77,7 @@ def photo(message):
 
 @bot.message_handler(content_types=['audio','voice'])
 def recording(message):
-    if message.from_user.id != 1928616895:
+    if message.from_user.id not in ALLOWED_IDS:
         return
     print(message)
     bot.reply_to(message, 'Got audio to transcribe. Working... ')
@@ -104,5 +99,6 @@ def recording(message):
         bot.reply_to(message, text)
     else:
         bot.reply_to(message, 'Sorry, no text recognized')
+
 
 bot.polling()
